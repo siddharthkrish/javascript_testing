ecf7a462b1822eb1871e140262a0328c45bb9cbe
Sid <siddharth.krish@moneysmart.co>
2017-07-13 16:37:02 +0800

    null checks

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum = require('./sum')
 test('adds 1 + 2 to equal 3',() => {
     expect(sum(1, 2)).toBe(3)
 })
+
+test('adds 1 + null to equal 1',() => {
+    expect(sum(1, null)).toBe(1)
+})
+
+test('adds null + 1 to equal 1',() => {
+    expect(sum(null, 1)).toBe(1)
+})
