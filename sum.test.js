8c2b3d36be65d6907e4e1977cd5856569f88ccea
Sid <siddharth.krish@moneysmart.co>
2017-07-13 16:16:49 +0800

    fix tests

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,11 @@ const sum = require('./sum')
 test('adds 1 + 2 to equal 3',() => {
     expect(sum(1, 2)).toBe(3)
 })
+
+test('adds 1 + 100 to equal 101',() => {
+    expect(sum(1, 100)).toBe(101)
+})
+
+test('adds 1 + 101 to equal 101',() => {
+    expect(sum(1, 2)).toBe(101)
+})
